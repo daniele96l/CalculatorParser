0ed5263aed932e1bcfee8ad5828cd610273cc9fc
daniele96l <daniele96ligato@gmail.com>
2021-05-06 01:02:49 +0200

    start

---
+++ shell.py
@@ -0,0 +1,11 @@
+
+import parser
+
+import parserOriginal
+
+while True:
+    text = input('basic > ')
+    result, error = parserOriginal.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
